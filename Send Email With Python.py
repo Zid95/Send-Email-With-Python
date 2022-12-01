89b6e2e04464a15610c123f5448bd4f8212e17ad
Zid95 <zid.abdulkarim@gmail.com>
2022-12-01 21:18:13 +0100

    Project Finshed

---
+++ Send Email With Python.py
@@ -0,0 +1,29 @@
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+email_sender = input("Enter Email For Sender: ").strip().lower()
+email_password = input("Enter App Passwords: ").strip()
+
+email_reciver = input("Enter Email For Reciver: ").strip().lower()
+
+subject = input("Enter The Email Subject: ").strip()
+
+body = input("Enter The Email Body: ").strip()
+
+
+em = EmailMessage()
+
+em['From'] = email_sender
+
+em['To'] = email_reciver
+
+em['subject'] = subject
+
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_reciver, em.as_string())
